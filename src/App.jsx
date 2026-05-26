3cdf5806a9ba7599654c9bed195816e58cd1886a
Oloruntopaul <paulolorunto@gmail.com>
2026-05-26 19:19:02 +0100

    Set basic landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 function App() {
   return (
-    <div className= "text-center p-8">
-      <h1 className= "text-3xl font-bold text-blue-600">
-        SafeNest
-      </h1>
-      <p className= "mt-4 text-gray-500">
-        Frontend is Live
-      </p>
+    <div className="text-center p-8">
+      <h1 className="text-3xl font-bold text-blue-600">SafeNest</h1>
+      <p className="mt-4 text-gray-500">Frontend is Live</p>
     </div>
   )
 }
