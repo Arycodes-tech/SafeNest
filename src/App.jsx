831475f79974a603091aa89ebde56ddbeb3bdc07
Laila <lailau996@gmail.com>
2026-06-07 18:25:38 +0100

    implement Forgot Password

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
+import ForgotPassword from "./components/ForgotPassword";
 function App() {
   return (
-    <div className= "text-center p-8">
-      <h1 className= "text-3xl font-bold text-blue-600">
-        SafeNest
-      </h1>
-      <p className= "mt-4 text-gray-500">
-        Frontend is Live
-      </p>
-    </div>
+<ForgotPassword />
   )
 }
 export default App
